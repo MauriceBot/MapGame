4d17ae5eb2263500fa2aacc200b2e4b32fdc4fe7
MauriceBot <nikotammentie@gmail.com>
2018-06-29 17:44:02 +0300

    INITIAL COMMIT

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AmChartsModule } from '@amcharts/amcharts3-angular';
+import { MatTabsModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+import { CharacterComponent } from './character/character.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    CharacterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AmChartsModule,
+    MatTabsModule,
+    BrowserAnimationsModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
